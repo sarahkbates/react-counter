7870c14f7a52a142943ab937084ac7d6b94ea917
Sarah Bates <skbates00@me.com>
2020-06-03 10:32:35 -0500

    up button doesn't work yet

--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function ButtonPad() {
+export default function ButtonPad({incrementCount, decrementCount, resetCount}) {
     return (
         <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
+            <button onCLick = {incrementCount}>Up Button</button>
+            <button onClick = {decrementCount}>Down Button</button>
+            <button onClick = {resetCount}>Reset Button</button>
         </div>
-    )
+    );
 }
